a9d858d93a824e7cc1871ec5291afbea8d5dcc9a
Ganesh Hasda <156532313+ganeshhep@users.noreply.github.com>
2024-10-22 02:09:05 +0530

    Add files via upload

--- events_029930132.root/Jet_Flavour/jet_flavour.cc
+++ events_029930132.root/Jet_Flavour/jet_flavour.cc
@@ -23,26 +23,26 @@ std::vector<fastjet::PseudoJet> createPseudoJets(ROOT::RVec<double>& px,
                                                  ROOT::RVec<double>& pz, 
                                                  ROOT::RVec<double>& e,
                                                  ROOT::RVec<int>& id) {
-    std::vector<fastjet::PseudoJet> jets;
+  std::vector<fastjet::PseudoJet> jets;
  
-    for (size_t i = 0; i < px.size(); ++i) {
+  for (size_t i = 0; i < px.size(); ++i) {
 
-      fastjet::PseudoJet jet(px[i], py[i], pz[i], e[i]);
-      jet.set_user_info(new fastjet::contrib::FlavHistory(id[i])); 
+    fastjet::PseudoJet jet(px[i], py[i], pz[i], e[i]);
+    jet.set_user_info(new fastjet::contrib::FlavHistory(id[i])); 
       
-      jets.push_back(jet);
-    }
+    jets.push_back(jet);
+  }
 
-    return jets;
+  return jets;
 }
 
 std::vector<fastjet::PseudoJet> applyAntiKtAlgorithm(std::vector<fastjet::PseudoJet>& particles, double R) {
    
-    fastjet::JetDefinition jet_def(fastjet::antikt_algorithm, R);
+  fastjet::JetDefinition jet_def(fastjet::antikt_algorithm, R);
 
-    std::vector<fastjet::PseudoJet> jets = jet_def(particles);
+  std::vector<fastjet::PseudoJet> jets = jet_def(particles);
 
-    return sorted_by_pt(jets);
+  return sorted_by_pt(jets);
 }
 
 std::vector<fastjet::PseudoJet> applyFlavouredAntiKtAlgorithm(std::vector<fastjet::PseudoJet>& particles, double R) {
@@ -55,6 +55,21 @@ std::vector<fastjet::PseudoJet> applyFlavouredAntiKtAlgorithm(std::vector<fastje
   std::vector<fastjet::PseudoJet> flav_jets = jet_def(particles);
 
   return sorted_by_pt(flav_jets);
+}
+
+std::string GetFirstElement(const std::string &entry) {
+
+  auto start = entry.find_first_of('[') + 1;
+  auto end = entry.find_first_of(' ', start);
+
+  if (end == std::string::npos) {
+
+    end = entry.find_first_of(']', start);
+
+  }
+
+  return entry.substr(start, end - start);
+
 }
 
 void jet_flavour()
@@ -97,11 +112,59 @@ void jet_flavour()
                               for (const auto& jet : jets) {
   		                descriptions.push_back(fastjet::contrib::FlavHistory::current_flavour_of(jet).description());}
 			      return descriptions;}, {"Flav_Jets"});
-  auto rdf = df14.Define("SDFlav_Description", [](std::vector<fastjet::PseudoJet>& jets) {
+  auto df15 = df14.Define("SDFlav_Description", [](std::vector<fastjet::PseudoJet>& jets) {
 			     std::vector<std::string> descriptions;
                              for (const auto& jet : jets) {
   		               descriptions.push_back(fastjet::contrib::FlavHistory::current_flavour_of(jet).description());}
 			     return descriptions;}, {"SDFlav_Jets"});
+  auto df16 = df15.Define("SDFJ", [](std::vector<std::string>& Flav_Description, std::vector<std::string>& SDFlav_Description, std::vector<fastjet::PseudoJet>& Flav_Jets) {  
+      std::vector<fastjet::PseudoJet> jet;
+      for (size_t i = 0; i < SDFlav_Description.size(); i++) {
+	std::string targetEntry = SDFlav_Description[i];
+        std::string targetElement = GetFirstElement(SDFlav_Description[i]);
+        int matchedEntry = -1; // Initialize with -1 to indicate no match by default
+	for (size_t j = 0; j < Flav_Description.size(); j++){
+	  if (Flav_Description[j] == targetEntry){
+	    matchedEntry = j;
+	    break;
+	  }
+	}
+	if (matchedEntry == -1) {
+        for (size_t j = 0; j < Flav_Description.size(); j++) {
+            if (GetFirstElement(Flav_Description[j]) == targetElement) {
+                matchedEntry = j; // Store the matched entry number
+                break; // Stop searching after finding the first match
+            }
+        }
+	}
+	jet.push_back(Flav_Jets[matchedEntry]);
+      }; return jet;}, {"Flav_Description", "SDFlav_Description", "Flav_Jets"});
+auto df17 = df16.Define("SDFlav_Dijets_Pt", [](const std::vector<fastjet::PseudoJet>& SDFlav_Jets){
+			  return (SDFlav_Jets[0] + SDFlav_Jets[1]).pt();}, {"SDFlav_Jets"});
+auto rdf = df17.Define("SDF_Dijets_Pt", [](const std::vector<fastjet::PseudoJet>& SDFJ){
+			                return (SDFJ[0] + SDFJ[1]).pt();}, {"SDFJ"});
+
+auto h1 = rdf.Histo1D({"pT", "SDFlav_Dijet", 400u, 0., 400.}, "SDFlav_Dijets_Pt");
+auto h2 = rdf.Histo1D({"pT", "SDF_Dijet", 400u, 0., 400.}, "SDF_Dijets_Pt");
+
+h1->GetXaxis()->SetTitle("pT[GeV]");
+h2->GetXaxis()->SetTitle("pT[GeV]");
+
+auto c1 = new TCanvas("c1");
+h1->DrawCopy();  
+
+  auto c2 = new TCanvas("c2");
+  h2->DrawCopy();
+
+  TH1D* hist1 = h1.GetPtr();
+  TH1D* hist2 = h2.GetPtr();
+
+  //print(type(hist0));
+
+  TH1D* hist3 = (TH1D*)hist2->Clone("hist3");
+  hist3->Divide(hist1);
+  auto c3 = new TCanvas("c3");
+  hist3->DrawCopy();
 
   auto displayOutput_1 = rdf.Display("Flav_Description");
 
